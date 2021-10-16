07ca58d1cf9904adb706c9a92390ff8d44175009
Paul Rybitskyi <Paul.Rybitskyi@gmail.com>
2021-10-16 23:52:13 +0300

    Clean up the project (#13)

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -20,13 +20,12 @@ plugins {
 }
 
 android {
-    compileSdkVersion(appConfig.compileSdkVersion)
-    buildToolsVersion(appConfig.buildToolsVersion)
+    compileSdk = appConfig.compileSdkVersion
 
     defaultConfig {
         applicationId = appConfig.applicationId
-        minSdkVersion(appConfig.minSdkVersion)
-        targetSdkVersion(appConfig.targetSdkVersion)
+        minSdk = appConfig.minSdkVersion
+        targetSdk = appConfig.targetSdkVersion
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -42,7 +41,7 @@ android {
         viewBinding = true
     }
 
-    lintOptions {
+    lint {
         isAbortOnError = false
     }
 
