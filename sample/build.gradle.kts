ef8eab5b8701b48a91858e8b35dd96aea42edfc3
Paul Rybitskyi <Paul.Rybitskyi@gmail.com>
2021-10-17 01:21:53 +0300

    Add git pre-push hook (#16)

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -68,3 +68,12 @@ dependencies {
     testImplementation(deps.jUnit)
     androidTestImplementation(deps.jUnitExt)
 }
+
+val installGitHook by tasks.registering(Copy::class) {
+    from(File(rootProject.rootDir, "hooks/pre-push"))
+    into(File(rootProject.rootDir, ".git/hooks/"))
+    // https://github.com/gradle/kotlin-dsl-samples/issues/1412
+    fileMode = 0b111101101 // -rwxr-xr-x
+}
+
+tasks.getByPath(":sample:preBuild").dependsOn(installGitHook)
